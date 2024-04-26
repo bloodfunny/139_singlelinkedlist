803cc75490821dad0ecb00ce40b6b9924e896f28
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-04-26 11:11:11 +0700

    membuat tampilan prosedur nim

--- 139_singlelinkedlist/139_singlelinkedlist.cpp
+++ 139_singlelinkedlist/139_singlelinkedlist.cpp
@@ -2,11 +2,33 @@
 using namespace std;
 
 struct node {
-	int mhs;
+	int noMhs;
 	string name;
 	node* next;
 };
 
 node* START = NULL;
 
+void addnode() {
+	int nim;
+	string nama;
+	node* nodeBaru = new node();
+	cout << "Masukan NIM: ";
+	cin >> nim;
+	cout << "Masukkan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
 
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+}
